e2fd0cd49c9a1037be2e38bfb6957db6ba01ec08
gauravkumarkamal <gauravkumarkamal@gmail.com>
2015-11-02 23:01:11 +0530

    changes random

--- api/index.js
+++ api/index.js
@@ -8,3 +8,14 @@ var connection = mysql.createConnection({
   database : 'mi_2015'
 });
 
+connection.connect();
+
+connection.query('SELECT * from < table name >', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+connection.end();
+
